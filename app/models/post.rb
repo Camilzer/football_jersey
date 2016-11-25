e0c20d94b11d3d23857677a0aacc4e8fd1c692d9
adelharchi <adel.harchi@gmail.com>
2016-11-25 03:24:31 +0100

    deploy

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/public/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	def self.search(search)
   		where("title ILIKE ?", "%#{search}%")
